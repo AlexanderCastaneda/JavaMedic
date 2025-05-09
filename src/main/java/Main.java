fbcd4e1c7aa136441cfdbd12e070aa8d13d4a174
Troyano <llvpsllartemis@gmail.com>
2025-05-08 20:19:11 -0500

    estas es una actualizacion de alexander

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Aprendiendo git con java, se sugre en conexiones remotas =D");
+        System.out.println("Aprendiendo git con java, se sugre en conexiones remotas =D, a partir de este punto alexander modifica el codigo");
     }
 }
